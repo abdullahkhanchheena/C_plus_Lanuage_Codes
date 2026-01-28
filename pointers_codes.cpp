f9b636c2dd35425ea2d2109d25db437fafbd8e3e
abdullahkhanchheena <abdullahkhanchheena@gmail.com>
2026-01-28 22:20:01 +0500

    Updated files

--- pointers_codes.cpp
+++ pointers_codes.cpp
@@ -31,6 +31,8 @@
 //     return 0;
 // }
 
+////=============================//
+
 ///=====================DYNAMIC MEMORY ALLOCATION==================//
 
 // #include <iostream>
@@ -135,3 +137,107 @@
 // }
 
 /////=================================================//
+
+//// //////=========Swaping two numbers and findinmg mean range normalization===========//
+// #include <iostream>
+// using namespace std;
+
+// //==============Function of swaping===================
+// void swap(int *p1, int *p2)
+// {
+//     int temp = *p1;
+//     *p1 = *p2;
+//     *p2 = temp;
+// }
+// //============Function of Range===========
+// void rangefunc(int *a, int *b, int *range)
+// {
+//     *range = *a - *b;
+// }
+// //============Function of Mean===========
+// void meanfunc(int *a, int *b, int *mean)
+// {
+//     *mean = (*a + *b) / 2.0;
+// }
+// //============Function of Normalization===========
+// void normalization(int *a, int *b)
+// {
+//     cout << "Normalization of first Number = " << (*a) / (*a) << endl;
+//     cout << "Normalization of first Number = " << (*b) / (*b) << endl;
+// }
+// int main()
+// {
+//     int a, b;
+//     cout<<"Input:";
+//     cout << "\nEnter First Value = ";
+//     cin >> a;
+//     cout << "Enter second Value = ";
+//     cin >> b;
+//     cout<<"Output:"<<endl;
+//     cout << "Values before swap:" << endl;
+//     cout << "a = " << a << endl;
+//     cout << "b = " << b << endl;
+
+//     //==========Calling SwapFunctin==========
+//     swap(&a, &b);
+//     cout << "Values after Swaping:" << endl;
+//     cout << "a = " << a << endl;
+//     cout << "b = " << b << endl;
+
+//     //==========Calling of Range==========
+//     int range = 0;
+//     rangefunc(&a, &b, &range);
+//     cout << "Range = " << range << endl;
+//     //==========Calling of Mean==========//
+//     int mean;
+//     meanfunc(&a, &b, &mean);
+//     cout << "Mean = " << mean << endl;
+
+//     //==========Calling SwapFunction==========
+//     normalization(&a, &b);
+//     return 0;
+// }
+
+//// //========================================================//
+
+/// //=================Finding total marls and percentage=====/
+
+// #include<iostream>
+// using namespace std;
+// //=========FUNCTION OF TOTAL MARKS====//
+// void marksfunc(int *sub1,int *sub2,int *sub3,int *totalmarks)
+// {
+// 	*totalmarks = *sub1+*sub2+*sub3;
+// }
+// //=========FUNCTION OF PERCENTAGE====//
+// void percentagefunc(int *totalmarks,float *percentage)
+// {
+// 	*percentage = (*totalmarks/300.0) * 100;
+// }
+// //===============MAIN FUNCATION===========//
+// int main()
+// {
+// 	//Declaring subjects
+// 	int sub1,sub2,sub3;
+// 	cout<<"Input:\n";
+// 	cout<<"Enter Marks of Subject 1 = ";
+// 	cin>>sub1;
+// 	cout<<"Enter Marks of Subject 2 = ";
+// 	cin>>sub2;
+// 	cout<<"Enter Marks of Subject 3 = ";
+// 	cin>>sub3;
+// 	cout<<"Output:\n";
+
+// //=========CALLING MARKS FUNCTIONS========//
+// 	int totalmarks;
+// 	marksfunc(&sub1,&sub2,&sub3,&totalmarks);
+// 	cout<<"Total Marks = "<<totalmarks<<endl;
+
+// //=========CALLING PERCENTAGE FUNCTION=====//
+// 	float percentage;
+// 	percentagefunc(&totalmarks,&percentage);
+// 	cout<<"Percentage = "<<percentage<<"%%"<<endl;
+// 	return 0;
+// }
+
+/// ///==================================================//
