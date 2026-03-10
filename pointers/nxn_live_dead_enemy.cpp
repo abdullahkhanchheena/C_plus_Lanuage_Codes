e3358cd6e6777f2e2a8cd9a6ddd22411b48fd881
abdullahkhanchheena <abdullahkhanchheena@gmail.com>
2026-03-10 23:16:31 +0500

    Updated Files

--- pointers/nxn_live_dead_enemy.cpp
+++ pointers/nxn_live_dead_enemy.cpp
@@ -10,7 +10,7 @@ int main()
 
     int **grid = new int *[n]; // ALLOCATING DYNAMIC MEMORY
 
-    cout << "Enter grid Elements: "<<endl;
+    cout << "Enter grid Elements: " << endl;
     for (int i = 0; i < n; i++)
     {
         grid[i] = new int[n];
@@ -18,11 +18,12 @@ int main()
         {
             cin >> grid[i][j];
         }
-        // cout << endl;
     }
 
     // INITIAL GRID
-    cout << "\nInitial Grid: \n";
+    cout << "\n----------------------" << endl;
+    cout << "Initial Grid: \n";
+    cout << "------------------------" << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -31,26 +32,26 @@ int main()
         }
         cout << endl;
     }
+    cout << "----------------------" << endl;
 
-    int total = n * n, hits = 0;
-    // CHECKING FOR LIVE ENEMIES
-    while (total > 0)
+    // HITTING ENEMIES
+    int total_hits = 0, attack_Index;
+    while (total_hits != n)
     {
-        int row, col;
-        cout << "Enter Row and Column to Attack: ";
-        cin >> row >> col;
-
-        if (grid[row][col] == 1)
+        for (int i = 0; i < n; i++)
         {
-            grid[row][col] = 0;
-            hits++;
-            total--;
+            for (int j = 0; j < n; j++)
+            {
+                cout << "Enter Index to Attack Enemy: ";
+                cin >> attack_Index[j][i];
+            }
         }
     }
 
     // OUTPUT
-    cout << "\nTotal hits: " << hits << endl;
+    cout << "\n----------------------" << endl;
     cout << "Updated Grid: " << endl;
+    cout << "----------------------" << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -59,7 +60,7 @@ int main()
         }
         cout << endl;
     }
-
+    cout << "----------------------" << endl;
     // DEALLOCATING MEMORY
     for (int i = 0; i < n; i++)
     {
